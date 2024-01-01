6b8c7d643254f248e0a6505f01e0a2d52a2c0834
littlegreen-games <155337105+littlegreen-games@users.noreply.github.com>
2024-01-01 14:39:17 -0800

    update index 2024

---
+++ 2023.js
@@ -0,0 +1,162 @@
+let data = [
+    {
+        id: 0, title: "Axie Tri-Force", description: 
+        "This casual game combines Little Green Games and SkyMavis, with an axie character inspired by the popular SkyMavis game Axie Infinity ",
+        image: "./crossads/images/com.SkyMavis.AxieMiniTriforce.png",
+        imageGooglePlay: "./imageWeb/gitbook.png",
+        linkGooglePlay: "https://littlegreen.gitbook.io/axie-mini-tri-force/",
+        textHeaderGooglePlay: "Gitbook", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/skymavis.png",
+        textHeaderAppStore: "Mavis Hub", textMoreAppStore: "(available)",
+        linkAppStore: "https://welcome.skymavis.com/download/",
+    },
+    {
+        id: 1, title: "Only Way Is UP", description: 
+        "Prepare to push your limits and embark on an adrenaline-fueled adventure in Only Way Is UP - Climb Parkour! This dynamic and heart-racing game is the perfect fusion of parkour and climbing, offering an exhilarating experience that will keep you on the edge of your seat.",
+        image: "./crossads/images/com.onlywayisup.climbparkour.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "https://drive.google.com/file/d/14bXehyUJxSm1QWwOIV0xBkdY9aqVcE-k/view?usp=sharing",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/itchio.png",
+        textHeaderAppStore: "Website", textMoreAppStore: "(available)",
+        linkAppStore: "https://littlegreengames.itch.io/only-way-is-up",
+    },
+    {
+        id: 2, title: "Tank vs Cube", description: 
+        "Popular game, gives the most relaxing feeling when destroying things. Shooting to destroy cubes feels super relaxing. Easy merge defense with numbers. Broken, bang.. bang.. bang ",
+        image: "./crossads/images/com.Hanakuin.TankvsCube.MergeShooter.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "https://drive.google.com/file/d/1plwaCQnodzDeJLsHW71fid6cjeWGm5yp/view?usp=sharing",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/applestore.png",
+        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
+        linkAppStore: "./",
+    },
+    {
+        id: 3, title: "Zooma Classic", description: 
+        "Match 3 or more marbles to prevent the door from closing. Use your spells to break as many marbles as you can. Mysterious treasures are waiting for you to get them, go through many ranks and rewards for outstanding people.",
+        image: "./crossads/images/com.Hanakuin.MarblesLegend.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "https://drive.google.com/file/d/1D0XGpvHLfcuOzAuYKKG1Oahs3jdADKH8/view?usp=sharing",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/applestore.png",
+        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
+        linkAppStore: "./",
+    }
+    ,
+    {
+        id: 4, title: "Gun Simulator", description: 
+        "Gun Simulator: Experience the exhilaration of a lifelike virtual firearm world. Immerse yourself in this thrilling game, where a vast selection of meticulously designed guns awaits.",
+        image: "./crossads/images/com.GunSimulator.GunSound.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/applestore.png",
+        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
+        linkAppStore: "./",
+    },
+    {
+        id: 5, title: "Mini Farm", description: 
+        "Mini Farm: Idle Tycoon, easy relaxing, addictive. Take care of pets and expand. Buy and take care of livestock: chickens, ducks, pigs, sheep, ..",
+        image: "./crossads/images/com.Hanakuin.MiniFarm.IdleTycoon.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "https://drive.google.com/file/d/1sgWW-ZwRK9Fnb75dM4Q_4aOAAMT9kW1X/view?usp=sharing",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/applestore.png",
+        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
+        linkAppStore: "./",
+    },
+    {
+        id: 6, title: "Zumbla Jungle", description: 
+        "Lands with many elements: fire, snow, desert, forest. Your mission is to open many new lands. Shoot marbles of the same color to destroy them. Let's go! Zumbla: Marbles Jungle!",
+        image: "./crossads/images/com.Zumbla.Marbles.Jungle.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "https://drive.google.com/file/d/1iC0pq91Bz-72vOrzLGFrlXwxpno-o3FP/view?usp=sharing",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/applestore.png",
+        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
+        linkAppStore: "./",
+    },
+    {
+        id: 7, title: "Cute Animals", description: 
+        "Cute Animals is a popular and unique puzzle game with different styles. It has tons of animals with cute graphics.",
+        image: "./crossads/images/com.Hanakuin.CuteAnimals.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/applestore.png",
+        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
+        linkAppStore: "./",
+    },
+    {
+        id: 8, title: "Zombie Survival 5", description: 
+        "Use 1 finger to move the character, zombies will be automatically targeted and destroyed. Get rewards and upgrade your character stronger.",
+        image: "./crossads/images/com.Hanakuin.ZombieSurvival5.png",
+        imageGooglePlay: "./imageWeb/googleplay.png",
+        linkGooglePlay: "https://drive.google.com/file/d/1_dAbNZtEPFQmYE1HBzcRuRZD69ReBqiJ/view?usp=sharing",
+        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
+        imageAppStore: "./imageWeb/applestore.png",
+        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
+        linkAppStore: "./",
+    }
+];
+
+
+let renderHtml = function (arr) {
+    let html = "";
+    arr.map((item, index) => {
+        html += `
+        <div class="row boder mt-4" style = "background-color: #ffffff">
+               <div class="col-lg-3 " style=" padding: 25px">
+                        <div class=" ">
+                            <img style="border-radius: 10px; width: 70%;"
+                                class="img-fluid "
+                                alt=""
+                                src="${item.image}">
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="row text-left mt-4">
+                            <div class="text-left">
+                                <h4>${item.title}</h4>
+                            </div>
+                            <div>
+                                <p style = "font-size: 12px;">${item.description}</p>
+                            </div>
+                        </div>
+                        <div class="row text-left mt-2" id="listButton">
+                            <div class="col-lg-4" onclick="openLink(this)" data-link="${item.linkGooglePlay}">
+                                <div class="btn btn-info button" style="">
+                                    <div style="padding: 5px;">
+                                        <img src="${item.imageGooglePlay}" alt="" style="width: 30px; height: 30px; padding: 5px">
+                                    </div>
+                                    <div style="margin-left: 30px;">
+                                        <span style="font-size: 14px; padding: 0px;margin: 0px;">${item.textHeaderGooglePlay}</span>
+                                        <p style="font-size: 12px;padding: 0px; margin: -5px;">${item.textMoreGooglePlay}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4" onclick="openLink(this)" data-link="${item.linkAppStore}">
+                                <div class="btn btn-info button" style="">
+                                    <div style="padding: 5px;">
+                                        <img src="${item.imageAppStore}" alt="" style="width: 30px; height: 30px; padding: 5px">
+                                    </div>
+                                    <div style="margin-left: 30px;">
+                                        <span style="font-size: 14px; padding: 0px; margin: 0px;">${item.textHeaderAppStore}</span>
+                                        <p style="font-size: 12px;padding: 0px;margin: -5px;">${item.textMoreAppStore}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+        </div>
+        `
+    })
+    $("#content").append(html);
+}
+renderHtml(data);
+let openLink = function (item) {
+    let link = $(item).data("link");
+    window.open(link)
+}
--- index.js
+++ index.js
@@ -1,104 +1,15 @@
 let data = [
-    {
-        id: 0, title: "Axie Tri-Force", description: 
-        "This casual game combines Little Green Games and SkyMavis, with an axie character inspired by the popular SkyMavis game Axie Infinity ",
-        image: "./crossads/images/com.SkyMavis.AxieMiniTriforce.png",
-        imageGooglePlay: "./imageWeb/gitbook.png",
-        linkGooglePlay: "https://littlegreen.gitbook.io/axie-mini-tri-force/",
-        textHeaderGooglePlay: "Gitbook", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/skymavis.png",
-        textHeaderAppStore: "Mavis Hub", textMoreAppStore: "(available)",
-        linkAppStore: "https://welcome.skymavis.com/download/",
-    },
-    {
-        id: 1, title: "Only Way Is UP", description: 
-        "Prepare to push your limits and embark on an adrenaline-fueled adventure in Only Way Is UP - Climb Parkour! This dynamic and heart-racing game is the perfect fusion of parkour and climbing, offering an exhilarating experience that will keep you on the edge of your seat.",
-        image: "./crossads/images/com.onlywayisup.climbparkour.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "https://drive.google.com/file/d/14bXehyUJxSm1QWwOIV0xBkdY9aqVcE-k/view?usp=sharing",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/itchio.png",
-        textHeaderAppStore: "Website", textMoreAppStore: "(available)",
-        linkAppStore: "https://littlegreengames.itch.io/only-way-is-up",
-    },
-    {
-        id: 2, title: "Tank vs Cube", description: 
-        "Popular game, gives the most relaxing feeling when destroying things. Shooting to destroy cubes feels super relaxing. Easy merge defense with numbers. Broken, bang.. bang.. bang ",
-        image: "./crossads/images/com.Hanakuin.TankvsCube.MergeShooter.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "https://drive.google.com/file/d/1plwaCQnodzDeJLsHW71fid6cjeWGm5yp/view?usp=sharing",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/applestore.png",
-        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
-        linkAppStore: "./",
-    },
-    {
-        id: 3, title: "Zooma Classic", description: 
-        "Match 3 or more marbles to prevent the door from closing. Use your spells to break as many marbles as you can. Mysterious treasures are waiting for you to get them, go through many ranks and rewards for outstanding people.",
-        image: "./crossads/images/com.Hanakuin.MarblesLegend.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "https://drive.google.com/file/d/1D0XGpvHLfcuOzAuYKKG1Oahs3jdADKH8/view?usp=sharing",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/applestore.png",
-        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
-        linkAppStore: "./",
-    }
-    ,
-    {
-        id: 4, title: "Gun Simulator", description: 
-        "Gun Simulator: Experience the exhilaration of a lifelike virtual firearm world. Immerse yourself in this thrilling game, where a vast selection of meticulously designed guns awaits.",
-        image: "./crossads/images/com.GunSimulator.GunSound.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/applestore.png",
-        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
-        linkAppStore: "./",
-    },
-    {
-        id: 5, title: "Mini Farm", description: 
-        "Mini Farm: Idle Tycoon, easy relaxing, addictive. Take care of pets and expand. Buy and take care of livestock: chickens, ducks, pigs, sheep, ..",
-        image: "./crossads/images/com.Hanakuin.MiniFarm.IdleTycoon.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "https://drive.google.com/file/d/1sgWW-ZwRK9Fnb75dM4Q_4aOAAMT9kW1X/view?usp=sharing",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/applestore.png",
-        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
-        linkAppStore: "./",
-    },
-    {
-        id: 6, title: "Zumbla Jungle", description: 
-        "Lands with many elements: fire, snow, desert, forest. Your mission is to open many new lands. Shoot marbles of the same color to destroy them. Let's go! Zumbla: Marbles Jungle!",
-        image: "./crossads/images/com.Zumbla.Marbles.Jungle.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "https://drive.google.com/file/d/1iC0pq91Bz-72vOrzLGFrlXwxpno-o3FP/view?usp=sharing",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/applestore.png",
-        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
-        linkAppStore: "./",
-    },
-    {
-        id: 7, title: "Cute Animals", description: 
-        "Cute Animals is a popular and unique puzzle game with different styles. It has tons of animals with cute graphics.",
-        image: "./crossads/images/com.Hanakuin.CuteAnimals.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/applestore.png",
-        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
-        linkAppStore: "./",
-    },
-    {
-        id: 8, title: "Zombie Survival 5", description: 
-        "Use 1 finger to move the character, zombies will be automatically targeted and destroyed. Get rewards and upgrade your character stronger.",
-        image: "./crossads/images/com.Hanakuin.ZombieSurvival5.png",
-        imageGooglePlay: "./imageWeb/googleplay.png",
-        linkGooglePlay: "https://drive.google.com/file/d/1_dAbNZtEPFQmYE1HBzcRuRZD69ReBqiJ/view?usp=sharing",
-        textHeaderGooglePlay: "Google Play", textMoreGooglePlay: "(available)",
-        imageAppStore: "./imageWeb/applestore.png",
-        textHeaderAppStore: "Apple Store", textMoreAppStore: "(comming soon)",
-        linkAppStore: "./",
-    }
+    // {
+    //     id: 0, title: "Axie Tri-Force", description: 
+    //     "This casual game combines Little Green Games and SkyMavis, with an axie character inspired by the popular SkyMavis game Axie Infinity ",
+    //     image: "./crossads/images/com.SkyMavis.AxieMiniTriforce.png",
+    //     imageGooglePlay: "./imageWeb/gitbook.png",
+    //     linkGooglePlay: "https://littlegreen.gitbook.io/axie-mini-tri-force/",
+    //     textHeaderGooglePlay: "Gitbook", textMoreGooglePlay: "(available)",
+    //     imageAppStore: "./imageWeb/skymavis.png",
+    //     textHeaderAppStore: "Mavis Hub", textMoreAppStore: "(available)",
+    //     linkAppStore: "https://welcome.skymavis.com/download/",
+    // }
 ];
 
 
